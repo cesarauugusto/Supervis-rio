0d5bc7fd1cac3b3fe1e8ecc1a8a9e89c17734c22
César Augusto Victor <104315558+cesarauugusto@users.noreply.github.com>
2023-11-24 19:26:19 -0300

    Update opc_ua.py

--- opc_ua.py
+++ opc_ua.py
@@ -1,4 +1,4 @@
-from opcua import Client
+from opcua import  Client
 import time
 
 global i
@@ -22,7 +22,6 @@ def disconect_opc():
     return(i)
     
 def read_tags():
-    
     path_start_sinal = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.start_sinal')
     start_sinal = path_start_sinal.get_value()
     path_stop_sinal = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.stop_sinal')
@@ -31,23 +30,37 @@ def read_tags():
     emergencia = path_emergencia.get_value()
     path_sensor = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.sensor')
     sensor = path_sensor.get_value()
-    path_motor = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.motor_esteira')
+    path_motor = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.motor')
     motor = path_motor.get_value()
-    
-    return start_sinal,stop_sinal,emergencia,sensor,motor
+
+    return start_sinal,stop_sinal,sensor,motor,emergencia
 
 def write_start():
-    path_start_sinal = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.start_sinal')
-    var_type = path_start_sinal.get_data_type_as_variant_type()
-    path_start_sinal.set_value(True,var_type)
+    path_start_memoria = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.start')
+    var_type = path_start_memoria.get_data_type_as_variant_type()
+    path_start_memoria.set_value(True,var_type)
     time.sleep(1)
-    path_start_sinal.set_value(False,var_type)
+    path_start_memoria.set_value(False,var_type)
+    print('Ligando Motor...')
     
 def write_stop():
-    path_stop_sinal = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.stop_sinal')
-    var_type = path_stop_sinal.get_data_type_as_variant_type()
-    path_stop_sinal.set_value(True,var_type)
+    path_stop_memoria = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.stop')
+    var_type = path_stop_memoria.get_data_type_as_variant_type()
+    path_stop_memoria.set_value(True,var_type)
     time.sleep(1)
-    path_stop_sinal.set_value(False,var_type)
-    
-                   
+    path_stop_memoria.set_value(False,var_type)
+    print('Desligando Motor...')
+
+def write_emergencia_start():
+    path_start_emergencia = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.emergencia')
+    var_type = path_start_emergencia.get_data_type_as_variant_type()
+    path_start_emergencia.set_value(False,var_type)
+    time.sleep(1)
+    print('Emergencia acionada...')
+
+def write_emergencia_stop():
+    path_stop_emergencia = client.get_node('ns=4;s=|var|CODESYS Control Win V3.Application.PLC_PRG.emergencia')
+    var_type = path_stop_emergencia.get_data_type_as_variant_type()
+    path_stop_emergencia.set_value(True,var_type)
+    time.sleep(1)
+    print('Emergencia desativada...')
